4ebf872cff0692f196c9f31b9e8709c40a3bb5ef
Daniel A. Wagenaar <daw@caltech.edu>
2019-12-09 22:03:46 -0800

    Created script findmuck191209 to delete affected rows in solveq5elastic and render tables. The "bad" z0 values are:
    604
    704
    804
    1504
    1604
    1704
    1804
    1904
    2004
    2404
    2504
    2804
    2904
    3204
    3304
    3404
    4004
    4104
    4404
    4504
    4604
    4704
    4804
    4904
    5204
    5304
    5504
    5604
    5704
    6004
    6104
    6604
    6704
    6804
    7704
    7804
    7904
    8004
    8104

---
+++ oneoff/findmuck191209.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+# Finds subvolumes mucked up by trans pug in matchpointsq5
+# We need to find all subvolumes in which _any_ trans involved m1!=m2
+
+import aligndb
+
+db = aligndb.DB()
+ri = db.runinfo()
+
+zz0 = []
+
+for z0 in range(4, 9504, 100):
+    sv = ri.subvolume(z0, 200)
+    got = False
+    for k in range(1, len(sv)):
+        r1 = sv[k-1].r
+        r2 = sv[k].r
+        cnt = db.sel(f'''select count(*) from transrunmontq5
+        where ((r={r1} and r2={r2}) or (r={r2} and r2={r1}))
+        and m!=m2''')[0][0]
+        if cnt>0:
+            got = True
+            break
+    if got:
+        print(z0)
+        zz0.append(z0)
+
+
+with db.db.cursor() as c:
+    for z0 in zz0:
+        c.execute(f'delete from solveq5elastic where z0={z0}')
+        c.execute(f'delete from renderq5elasticdone where z>={z0} and z<{z0+200}')
+        c.execute(f'delete from renderq1done where z>={z0} and z<{z0+200}')    
+
+        
