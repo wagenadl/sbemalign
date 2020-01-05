a5aa4f815afe38bd02ca9381bdc897d8341c7bb6
Daniel A. Wagenaar <daw@caltech.edu>
2020-01-05 15:31:45 -0800

    inferruns finds out runinfo based on file system

--- inferruns.py
+++ inferruns.py
@@ -4,7 +4,9 @@ import aligndb
 import config
 import os
 
-def configureRun(r, z0):
+db = aligndb.DB()
+
+def runInfo(r):
     S = 0
     while True:
         if os.path.exists(config.rawtile(r, 0, S)):
@@ -17,15 +19,29 @@ def configureRun(r, z0):
             M = M + 1
         else:
             break
-    print(f'Run {R} Montages {M} Slices {S} Z0 {z0}')
-    return S
+    return (M, S)
+
+def createTable():
+    db.exe('drop table if exists runs')
+    db.exe('create table runs ( r integer, M integer, S integer, z0 integer )')
+
+def configureRun(r, M, S, z0):
+    print(f'Run {r} Montages {M} Slices {S} z0 {z0}')
+    db.exe(f'insert into runs (r,M,S,z0) values ({r}, {M}, {S}, {z0})')
+
+#################
+
+createTable()
 
 r = 1
 z0 = 0
             
 while True:
     if os.path.exists(config.rawtile(r, 0, 0)):
-        z0 += configureRun(r, z0)
+        M, S = runInfo(r)
+        configureRun(r, M, S, z0)
+        z0 += S
         r = r + 1
     else:
         break
+
