c7210aed2f489a4358c6812a7f80e1480a193523
Daniel Wagenaar <daw@caltech.edu>
2020-01-05 13:34:13 -0800

    zcombine now uses db table

--- zcombine.py
+++ zcombine.py
@@ -9,9 +9,25 @@ import pathlib
 import os
 import numpy as np
 import rawimage
+import aligndb
 
 Zmax = 9604 # max Z at B=0
 
+db = aligndb.DB()
+
+tbl = 'zcombine_done'
+
+def maketable():
+    db.exe(f'''create table if not exists {tbl} (
+    a integer,
+    b integer,
+    z integer
+    )''')
+
+
+def droptable():
+    db.nofail(f'drop table {tbl}')
+    
 def abpath(a, b, z=None, y=None, x=None):
     pth = f'{root}/A{a}B{b}'
     if not z is None:
@@ -22,27 +38,22 @@ def abpath(a, b, z=None, y=None, x=None):
             pth += f'/Y{y}'
             if x is not None:
                 pth += f'/X{x}.jpg'
-    #print(pth)
     return pth
 
 def inpath(a, z, y, x):
     zlo = z % 100
     zhi = z // 100
     pth = f'{root}/Z{zhi}/{zlo}/A{a}/Y{y}/X{x}.jpg'
-    #print(pth)
     return pth
 
 def zcombine(a, b, x, y, z):
     '''ZCOMBINE - Combines several z tiles
-   ZCOMBINE(a, b, x, y, z, b) loads 2^b tiles to construct the
-   averaged tile a/b/z/y/x from a/z0..z1/y/x.
-   ZCOMBINE is smart enough to divide by less than 2^b if some source
-   tiles are missing and doesn't save anything when there are zero source
-   tiles.
-   n = ZCOMBINE(...) returns the number of tiles, or -1 if previously done.'''
-
-    if pathlib.Path(abpath(a, b, z, y, x)).exists():
-        return -1
+    ZCOMBINE(a, b, x, y, z, b) loads 2^b tiles to construct the
+    averaged tile a/b/z/y/x from a/z0..z1/y/x.
+    ZCOMBINE is smart enough to divide by less than 2^b if some source
+    tiles are missing and doesn't save anything when there are zero source
+    tiles.
+    n = ZCOMBINE(...) returns the number of tiles, or -1 if previously done.'''
     
     Z = 2**b
     n = 0
@@ -72,32 +83,25 @@ def combinelevel(a, b, z):
     topany = False
     for y in range(0, Ymax):
         doneany = False
-        guard = pathlib.Path(abpath(a, b, z, y) + '/complete')
-        if not guard.exists():
-            for x in range(0, Xmax):
-                if zcombine(a, b, x, y, z):
-                    doneany = True
-            if doneany:
-                guard.touch()
-                topany = True
+        for x in range(0, Xmax):
+            if zcombine(a, b, x, y, z):
+                doneany = True
+        if doneany:
+            topany = True
     return topany
 
 def perhapscombinelevel(a, b, z):
-    guard = pathlib.Path(abpath(a, b, z) + '/complete')
-    if not guard.exists():
-        topany = combinelevel(a, b, z)
-        if topany:
-            guard.touch()
+    cnt = db.sel(f'select count(1) from {tbl} where a={a} and b={b} and z={z}')
+    if cnt[0][0]==0:
+        combinelevel(a, b, z)
+        db.exe(f'insert into {tbl} (a, b, z) values ({a}, {b}, {z})')
             
 def allcombine(a, b):
     Zmax1 = int(np.ceil(Zmax / 2**b)) # max Z at given B
-    guard = pathlib.Path(abpath(a, b) + '/complete')
-    if not guard.exists():
-        fac = factory.Factory(12)
-        for z in range(0, Zmax1):
-            fac.request(perhapscombinelevel, a, b, z)
-        fac.shutdown()
-        guard.touch()
+    fac = factory.Factory(12)
+    for z in range(0, Zmax1):
+        fac.request(perhapscombinelevel, a, b, z)
+    fac.shutdown()
 
 def infofile():
     txt = '''{
